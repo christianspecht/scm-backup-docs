b2852bb40d35531082b648b8870170a01bb7e59d
Christian Specht <christi@nspecht.de>
2016-12-19 22:53:28 +0100

    theme/logo

--- source/conf.py
+++ source/conf.py
@@ -83,7 +83,7 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'classic'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -95,6 +95,9 @@ html_theme = 'alabaster'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+
+html_logo = 'img/logo64x64.png'
 
 
 # -- Options for HTMLHelp output ------------------------------------------
