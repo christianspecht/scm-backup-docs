aa15dd9831891a789b731b109e914886f6db4adf
Christian Specht <christi@nspecht.de>
2017-01-08 20:55:45 +0100

    webmaster tools

--- source/conf.py
+++ source/conf.py
@@ -109,6 +109,8 @@ if not on_rtd:  # only import and set the theme if we're building docs locally
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
+# Files from this folder will be copied unchanged: http://www.sphinx-doc.org/en/latest/config.html#confval-html_extra_path
+html_extra_path = ['_files']
 
 html_logo = 'img/logo64x64.png'
 
