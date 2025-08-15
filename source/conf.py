8ae510655d5e98bf4bbb3d7620481617679e7220
Christian Specht <christi@nspecht.de>
2025-08-16 00:21:09 +0200

    remove theme flyout again
    Introduced in 3a0b84c2, removed now and RTD's default flyout enabled again
    because RTD's default flyout shows up in all older doc versions, but the theme flyout doesn't.

--- source/conf.py
+++ source/conf.py
@@ -128,9 +128,7 @@ html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
-html_theme_options = {
-    'flyout_display': 'attached'
-}
+html_theme_options = {}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
